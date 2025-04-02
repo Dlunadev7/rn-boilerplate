3f70b57d76fdbe945cbc62eea13d642949c043ec
David <dluna.dev@gmail.com>
2025-04-01 21:27:38 -0300

    feat(utils): create authRoutes util.

---
+++ src/utils/enum/auth.routes.ts
@@ -0,0 +1,11 @@
+export enum AuthRoutes {
+  SIGN_IN = "sign-in",
+  SIGN_UP = "sign-up",
+  RECOVERY_PASSWORD = "recovery-password",
+}
+
+export enum AuthRoutesLink {
+  SIGN_IN = "/(auth)/sign-in",
+  SIGN_UP = "/(auth)/sign-up",
+  RECOVERY_PASSWORD = "/(auth)/recovery-password",
+}
