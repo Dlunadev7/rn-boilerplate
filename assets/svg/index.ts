c3ea671e6b1d8236cdf59204d573ccac25bacac3
David <dluna.dev@gmail.com>
2025-04-02 16:22:26 -0300

    app: add new svg icons and metro configuration.

---
+++ assets/svg/index.ts
@@ -0,0 +1,17 @@
+import Check from './check.svg';
+import Cross from './cross.svg';
+import ArrowLeft from './arrow-left.svg';
+import Stars from './stars.svg';
+import Facebook from './facebook.svg';
+import Google from './google.svg';
+import Apple from './apple.svg';
+
+export {
+  Check,
+  Cross,
+  ArrowLeft,
+  Stars,
+  Facebook,
+  Google,
+  Apple
+}
