903ba199796da4160345343f10f8d4862257f904
David <dluna.dev@gmail.com>
2025-04-09 15:17:21 -0300

    app: add new environment vars, and packages.

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const EXPO_PUBLIC_COUNTRY_API = "https://restcountries.com/v3.1/all"
+export const EXPO_PUBLIC_MAPBOX_KEY = "pk.eyJ1IjoiZGF2aWRsdW5hMDkzMCIsImEiOiJjbTkwdTVib2IwajA3MnNxNzFpaTE0cnhjIn0.ioBl-AcbLxU81qFK4mY3Rg"
+export const PUBLIC_MAPBOX_API_URL = "https://api.mapbox.com/geocoding/v5/mapbox.places"
+export const PUBLIC_GOOGLE_MAPS_TOKEN = "AIzaSyAgRzfCFbjqNJCZgGBFTvidZtppIajN7BY"
